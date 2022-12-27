753afb679bc9d5607842ffb49ba1da6aad5be36e
Niksa Praljak <niksapraljak1@midway3-login4.rcc.local>
2022-12-27 14:53:21 -0600

    update novelty levenshtein shell scripts

---
+++ scripts/compute_design_novelty.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+python -V
+export DIR="$(dirname "$(pwd)")"
+#source activate torch_GPU
+export PYTHONPATH=${PYTHONPATH}:${DIR}
+
+
+# path variables
+export dataset_path='.././data/ACS_SynBio_SH3_dataset.csv'
+export output_results_path='.././outputs/novelty/SH3_DesignTrainDataset_MinLeven.csv'
+export SEED=42
+export option='design'
+
+python ../compute_dataset_novelty.py \
+		--dataset_path ${dataset_path} \
+		--output_results_path ${output_results_path} \
+		--SEED ${SEED} \
+		--option ${option} \
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
